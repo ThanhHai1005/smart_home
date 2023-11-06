c0e188ef97d42dfaf22d2fffe427ab876379bcea
Thanh Hai <haiduong.010102@gmail.com>
2023-11-07 00:47:35 +0700

    update 07 11 2023

--- trans.js
+++ trans.js
@@ -22,21 +22,19 @@ btn2.addEventListener('click', ()=>{
 
 
 
-let btn3 = document.querySelector('#btn3');
-let img2 = document.querySelector('#call');
-let btn4 = document.querySelector('#btn4');
-// functions nut bam
-btn3.addEventListener('click', ()=>{
-    img2.src = './img/phonerun.gif'; 
-    firebase.database().ref("thietbi2").set({dienthoai:1})
-})
+// let btn3 = document.querySelector('#btn3');
+// let img2 = document.querySelector('#call');
+// let btn4 = document.querySelector('#btn4');
+// // functions nut bam
+// btn3.addEventListener('click', ()=>{
+//     img2.src = './img/phonerun.gif'; 
+//     firebase.database().ref("thietbi2").set({dienthoai:1})
+// })
 
-btn4.addEventListener('click', ()=>{
-    img2.src = './img/mobile-1976104_1280.png';
-    firebase.database().ref("thietbi2").set({dienthoai:0})
-})
-
-
+// btn4.addEventListener('click', ()=>{
+//     img2.src = './img/mobile-1976104_1280.png';
+//     firebase.database().ref("thietbi2").set({dienthoai:0})
+// })
 
 
 
@@ -56,22 +54,24 @@ btn4.addEventListener('click', ()=>{
 
 
 
-// khoi tao cac thong s
-let btn5 = document.querySelector('#btn5');
-let img3 = document.querySelector('#loa');
-let btn6 = document.querySelector('#btn6');
 
-// functions nut bam
-btn5.addEventListener('click', ()=>{
-     img3.src = './img/loaon.gif' ;
-     firebase.database().ref("thietbi3").set({loa:1})
 
-})
-btn6.addEventListener('click', ()=>{
-    img3.src = './img/loa.png';
-    firebase.database().ref("thietbi3").set({loa: 0})
+// // khoi tao cac thong s
+// let btn5 = document.querySelector('#btn5');
+// let img3 = document.querySelector('#loa');
+// let btn6 = document.querySelector('#btn6');
 
-})
+// // functions nut bam
+// btn5.addEventListener('click', ()=>{
+//      img3.src = './img/loaon.gif' ;
+//      firebase.database().ref("thietbi3").set({loa:1})
+
+// })
+// btn6.addEventListener('click', ()=>{
+//     img3.src = './img/loa.png';
+//     firebase.database().ref("thietbi3").set({loa: 0})
+
+// })
 
 
 
@@ -80,7 +80,7 @@ btn6.addEventListener('click', ()=>{
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDgcEpfJfMIz3UzquAMH-Kee17H7Q9GL7Y",
+    apiKey: "avWxy4yyfaipVCy39zbMi6qk5ZmZPrcqG3oXqQq9",
     authDomain: "tt-iot-esp32.firebaseapp.com",
     databaseURL: "https://tt-iot-esp32-default-rtdb.firebaseio.com",
     projectId: "tt-iot-esp32",
@@ -99,16 +99,7 @@ firebase.database().ref("Do am 1").on("value", function (snapshot) {
     var data = snapshot.val();
     document.getElementById("doam1").innerHTML = "Humidity: " + data + "%";
 });
-// firebase.database().ref("Nhiet do 2").on("value", function (snapshot) {
-//     var data = snapshot.val();
-//     document.getElementById("nhietdo3").innerHTML = "Temperature: " + data.Nhietdo + "°C";
-//     document.getElementById("doam3").innerHTML = "Humidity: " + data.Doam + "%";
-// });
-// firebase.database().ref("HaNoi").on("value", function (snapshot) {
-//     var data = snapshot.val();
-//     document.getElementById("nhietdo2").innerHTML = "Temperature: " + data.Nhietdo + "°C";
-//     document.getElementById("doam2").innerHTML = "Humidity: " + data.Doam + "%";
-// });
+
 firebase.database().ref("Nhiet do 2").on("value", function (snapshot) {
     var data = snapshot.val();
     document.getElementById("nhietdo2").innerHTML = "Temperature: " + data + "°C";
